8f5e9c98421cd82b19b3734c0a1a00a31ea7fa79
小傅哥 <184172133@qq.com>
2023-11-04 10:43:11 +0800

    小傅哥，feat：分库分表路由组件 sharding-jdbc

---
+++ xfg-frame-archetype-std-infrastructure/src/main/java/cn/bugstack/infrastructure/persistent/dao/IUserOrderDao.java
@@ -0,0 +1,17 @@
+package cn.bugstack.infrastructure.persistent.dao;
+
+import cn.bugstack.infrastructure.persistent.po.UserOrderPO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface IUserOrderDao {
+
+    void insert(UserOrderPO userOrderPO);
+
+    void updateOrderStatusByUserId(String userId);
+
+    List<UserOrderPO> selectByUserId(String userId);
+
+}
