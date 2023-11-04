43268426d1debf741d899695d660cdb8879daee5
小傅哥 <184172133@qq.com>
2023-11-04 16:43:01 +0800

    小傅哥，feat：更新说明

---
+++ xfg-frame-archetype-std-trigger/src/main/java/cn/bugstack/trigger/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * http 提供服务
+ * job xxl-job 任务调度
+ * listener RocketMQ 监听
+ * rpc Dubbo 接口，实现RPC服务
+ */
+package cn.bugstack.trigger;
---
+++ xfg-frame-archetype-std-types/src/main/java/cn/bugstack/types/model/Response.java
@@ -0,0 +1,22 @@
+package cn.bugstack.types.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Response<T> implements Serializable {
+
+    private static final long serialVersionUID = 5130392244064623509L;
+
+    private String code;
+    private String info;
+    private T data;
+
+}
